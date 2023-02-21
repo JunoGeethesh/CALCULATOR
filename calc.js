47d7e1b074f914d448c61e8479da1eab0f7fe189
JunoGeethesh <junovalsaraj008@gmail.com>
2023-02-21 14:35:42 +0530

    JS-DOM CALCULATOR

---
+++ calc.js
@@ -0,0 +1,14 @@
+
+    function clearSreen(){
+        document.getElementById("result").value="";
+
+    }
+    function display(value){
+        document.getElementById("result").value += value;
+    }
+    function calculate(){
+        var a=document.getElementById("result").value; 
+        var b= eval(a);
+        document.getElementById("result").value=b;
+        console.log(b)
+    }
